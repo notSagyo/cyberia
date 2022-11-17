63690154f2b917569da383e0913e559df840667e
notSagyo <notsagyo@gmail.com>
2022-11-17 13:07:41 -0300

    Use cuePlaylist instead of loadPlaylist

--- components/CustomPlayer/CustomPlayerMain.tsx
+++ components/CustomPlayer/CustomPlayerMain.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback } from 'react';
 import ReactPlayer from 'react-player';
 import { loadSongFromStorage } from './CustomPlayerHelper';
 import styles from './CustomPlayerMain.module.scss';
@@ -10,47 +10,27 @@ import { MAIN_SONG_START_TIME } from '/data/songs';
 
 // If it's paused on storage then autoplay will be false
 const autoplay = !Boolean(Number(localStorage.getItem(storagePausedKey)));
-const song = loadSongFromStorage();
-// Helper variables
-let shouldPausePlaylist = false;
+const storedSong = loadSongFromStorage();
 let firstLoad = true;
 
-// ?TODO: Mute on playlist load to avoid initial chirp
 /** Always import this component with next/dynamic */
 const CustomPlayerMain = ({ onReady, ...props }: CustomPlayerProps) => {
-  const [ytPlayer, setYtPlayer] = useState<any>();
-
   // Load song from localstorage else load main song at specified time
   const handleReady = useCallback(
     (player: ReactPlayer) => {
       onReady && onReady(player);
-      if (song && firstLoad) {
-        player.seekTo(song.time);
+      if (storedSong && firstLoad) {
+        player.seekTo(storedSong.time);
         // Restore YouTube playlist state
-        if (song.source === 'youtube') {
-          const interalPLayer = player.getInternalPlayer();
-          const didUserInteract = new Audio('/sound/empty-audio.mp3').play();
-          interalPLayer?.loadPlaylist(song.url, song.songIndex, song.time);
-          didUserInteract.then(() => (shouldPausePlaylist = true));
-          setYtPlayer(interalPLayer);
-        }
+        if (storedSong.source === 'youtube')
+          player
+            .getInternalPlayer()
+            .cuePlaylist(storedSong.url, storedSong.songIndex, storedSong.time);
       } else if (firstLoad) player.seekTo(MAIN_SONG_START_TIME);
       firstLoad = false;
     },
     [onReady]
   );
-
-  useEffect(() => {
-    // Pause player when a playlist is loaded, if autoplay is set to false
-    if (autoplay || !song || !ytPlayer || song.source !== 'youtube') return;
-    const onStateChangeYt = (e: any) => {
-      if (!shouldPausePlaylist || e.data != 1) return;
-      ytPlayer?.pauseVideo();
-      shouldPausePlaylist = false;
-    };
-    ytPlayer.addEventListener('onStateChange', onStateChangeYt);
-    return () => ytPlayer.removeEventListener('onStateChange', onStateChangeYt);
-  }, [ytPlayer]);
 
   return (
     <div className={styles.mainPlayerContainer}>
