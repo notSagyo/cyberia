4b0022cf9581b8c8e52d8e77dd64274ec6a3908d
notSagyo <notsagyo@gmail.com>
2023-11-29 23:41:11 -0300

    Fix consumet's gogoanime; remove psych ward

--- services/consumet-service-server.ts
+++ services/consumet-service-server.ts
@@ -1,28 +1,29 @@
+import { ANIME, MANGA } from '@consumet/extensions';
 import { AnimeParser, MangaParser } from '@consumet/extensions/dist/models';
-import Gogoanime from '@consumet/extensions/dist/providers/anime/gogoanime';
-import Zoro from '@consumet/extensions/dist/providers/anime/zoro';
-import MangaDex from '@consumet/extensions/dist/providers/manga/mangadex';
-import Mangasee123 from '@consumet/extensions/dist/providers/manga/mangasee123';
 import {
-  animeProvider,
   AnimeProvidersNames,
-  mangaProvider,
   MangaProvidersNames,
+  animeProvider,
+  mangaProvider,
 } from './consumet-service';
+
+class Gogoanime extends ANIME.Gogoanime {
+  baseUrl = 'https://anitaku.to';
+}
 
 // Anime =====================================================================//
 export const animeProviders: Record<AnimeProvidersNames, AnimeParser> = {
   gogoanime: new Gogoanime(),
-  zoro: new Zoro(),
+  zoro: new ANIME.Zoro(),
   get default() {
     return this[animeProvider];
   },
 };
-
 // Manga =====================================================================//
+
 export const mangaProviders: Record<MangaProvidersNames, MangaParser> = {
-  mangadex: new MangaDex(),
-  mangasee123: new Mangasee123(),
+  mangadex: new MANGA.MangaDex(),
+  mangasee123: new MANGA.Mangasee123(),
   get default() {
     return this[mangaProvider];
   },
