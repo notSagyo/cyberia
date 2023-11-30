ec41967b793a78e896739d30284b1f5f57677524
notSagyo <notsagyo@gmail.com>
2023-11-30 15:19:55 -0300

    Make manga's chapter list not static

--- pages/read/manga/[mangaId]/index.tsx
+++ pages/read/manga/[mangaId]/index.tsx
@@ -1,28 +1,33 @@
 import { IMangaInfo } from '@consumet/extensions/dist/models';
-import { GetStaticPaths, GetStaticProps } from 'next';
-import { ParsedUrlQuery } from 'querystring';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import Layout from '/components/Layout/Layout';
 import LinkHeading from '/components/LinkHeading/LinkHeading';
 import LinkList from '/components/LinkList/LinkList';
 import mangas from '/data/mangas';
-import { mangaProviders } from '/services/consumet-service-server';
+import { getMangaInfo } from '/services/consumet-service';
 import { mangaURL, readURL } from '/utils/urls';
 import { toUrlikeString } from '/utils/utils';
 
-interface MangaIdProps {
-  mangaInfo: IMangaInfo;
-}
+const MangaIdPage = () => {
+  const [mangaInfo, setMangaInfo] = useState<IMangaInfo | null>(null);
+  let { mangaId: mangaIdParam } = useRouter().query;
+  const mangaId = mangaIdParam?.toString();
 
-interface iQueryParams extends ParsedUrlQuery {
-  mangaId: string;
-}
+  useEffect(() => {
+    mangaId &&
+      getMangaInfo(mangaId)
+        .then((res) => setMangaInfo(res))
+        .catch((err) => console.error(err));
+  }, [mangaId]);
 
-const MangaIdPage = ({ mangaInfo }: MangaIdProps) => {
-  const mangaId = mangaInfo.id;
-  const chapters = mangaInfo.chapters || [];
+  const chapters = mangaInfo?.chapters || [];
 
   const mangaName = toUrlikeString(
-    mangas.default.find((manga) => manga.id == mangaId)?.title || mangaId
+    mangas.default.find((manga) => manga.id == mangaId)?.title ||
+      mangaId ||
+      'manga',
   );
 
   return (
@@ -31,37 +36,20 @@ const MangaIdPage = ({ mangaInfo }: MangaIdProps) => {
         {mangaURL}/{mangaName}
       </LinkHeading>
 
-      <LinkList
-        links={chapters.map((chapter, i) => ({
-          href: `${mangaURL}/${mangaId}/${chapter.id}/1`,
-          title: `${mangaURL}/${mangaName}/${i + 1}`,
-          key: chapter.id,
-        }))}
-      />
+      {mangaInfo ? (
+        <LinkList
+          links={chapters.map((chapter, i) => ({
+            href: `${mangaURL}/${mangaId}/${chapter.id}/1`,
+            title: `${mangaURL}/${mangaName}/${i + 1}`,
+            key: chapter.id,
+          }))}
+        />
+      ) : (
+        <Image src="/img/loading.gif" alt="loading" width={384} height={51} />
+      )}
     </Layout>
   );
 };
 
 // Static stuff ==============================================================//
-export const getStaticProps: GetStaticProps<MangaIdProps> = async (context) => {
-  const { mangaId } = context.params as iQueryParams;
-  const mangaInfo = await mangaProviders.default.fetchMangaInfo(mangaId);
-  return {
-    props: {
-      mangaInfo: {
-        ...mangaInfo,
-        title: mangaInfo.title || '',
-        chapters: mangaInfo.chapters?.reverse(),
-      },
-    },
-  };
-};
-
-export const getStaticPaths: GetStaticPaths<iQueryParams> = async () => {
-  const paths = mangas.default.map((manga) => ({
-    params: { mangaId: manga.id },
-  }));
-  return { paths, fallback: false };
-};
-
 export default MangaIdPage;
--- services/consumet-service.ts
+++ services/consumet-service.ts
@@ -1,9 +1,19 @@
-export type AnimeProvidersNames = 'gogoanime' | 'zoro' | 'default';
+import { IMangaInfo } from '@consumet/extensions';
+import { getMangaInfoURL } from '/utils/urls';
 
+export type AnimeProvidersNames = 'gogoanime' | 'zoro' | 'default';
 export type MangaProvidersNames = 'mangadex' | 'mangasee123' | 'default';
 
 export const animeProvider: Exclude<AnimeProvidersNames, 'default'> =
   'gogoanime';
-
 export const mangaProvider: Exclude<MangaProvidersNames, 'default'> =
   'mangasee123';
+
+export const getMangaInfo = async (
+  mangaId: string,
+  provider: MangaProvidersNames = mangaProvider,
+): Promise<IMangaInfo> => {
+  return fetch(`${getMangaInfoURL(provider)}/${mangaId}`)
+    .then((res) => res)
+    .then((data) => data.json());
+};
